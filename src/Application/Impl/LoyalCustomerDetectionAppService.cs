4c40bd749d351b866e4d54d97b02ea14d47d7bca
Shawn Kuo <were1122was@gmail.com>
2026-01-28 14:27:51 +0800

    finish app service

--- src/Application/Impl/LoyalCustomerDetectionAppService.cs
+++ src/Application/Impl/LoyalCustomerDetectionAppService.cs
@@ -1,15 +1,19 @@
 using DomainModel.Interface;
 using DomainModel.Model;
+using DomainService.Interface;
 
 namespace Application.Impl
 {
     public class LoyalCustomerDetectionAppService
     {
         private readonly ILogRepository _logRepository;
+        private readonly ILoyalCheckDomainService _loyalCheckDomainService;
 
-        public LoyalCustomerDetectionAppService(ILogRepository logRepository)
+        public LoyalCustomerDetectionAppService(ILogRepository logRepository,
+                                                ILoyalCheckDomainService loyalCheckDomainService)
         {
             _logRepository = logRepository;
+            _loyalCheckDomainService = loyalCheckDomainService;
         }
 
         public List<string> GetLoyalCustomer(string sourcePath)
@@ -21,8 +25,11 @@ namespace Application.Impl
 
             var loyalCustomer = new List<string>();
 
-            // 目的 : 我想知道「每一個使用者，每一天都去過哪些頁面」
-            // step1: 遍歷每個使用者 step2: 將使用者使用過的日期先分組 step3: 將每組的日期跟使用造訪過哪些網站存在新的物件
+            // 目的 : 我想知道「每一個用戶，每一天都去過哪些頁面」
+            // step1: 遍歷每個用戶 
+            // step2: 將用戶使用過的日期再分組 
+            // step3: 將每組的日期跟用戶造訪過哪些網站存在新的物件
+            // step4: 儲存符合忠誠條件的用戶
             foreach (var user in usersGroup)
             {
                 var dailyActivity = user.GroupBy(log => log.TimeStamp.Date)
@@ -31,8 +38,13 @@ namespace Application.Impl
                                             Date = dateGroup.Key,
                                             VisitedPages = dateGroup.Select(x => x.Page).ToHashSet()
                                         }).ToList();
-                
+
+
+                if (_loyalCheckDomainService.IsLoyal(dailyActivity))
+                    loyalCustomer.Add(user.Key);
             }
+
+            return loyalCustomer;
         }
     }
 }
