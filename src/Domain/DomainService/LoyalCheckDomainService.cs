63c953d15ac8aceed6d47ecfc105340c661e4736
Shawn Kuo <were1122was@gmail.com>
2026-01-28 18:55:54 +0800

    refactor

--- src/Domain/DomainService/LoyalCheckDomainService.cs
+++ src/Domain/DomainService/LoyalCheckDomainService.cs
@@ -14,25 +14,22 @@ namespace DomainService
             var sort = dailyActivity.OrderBy(x => x.Date).ToList();
 
             // 往後找是否有連續的第二天與第三天，迴圈停止條件是序列總數減2，因為最後兩個元素沒辦法湊齊往後的三天
-            for(int i = 0; i < sort.Count - 2; i++)
+            for (int i = 0; i < sort.Count - 2; i++)
             {
                 var day1 = sort[i];
                 var day2 = sort.FirstOrDefault(x => x.Date == day1.Date.AddDays(1));
                 var day3 = sort.FirstOrDefault(x => x.Date == day1.Date.AddDays(2));
 
-                if(day2 == null || day3 == null)
-                    continue;
-                
-                var totalPages = new HashSet<string>();
-                foreach (var page in day1.VisitedPages)
-                    totalPages.Add(page);
-                foreach (var page in day2.VisitedPages)
-                    totalPages.Add(page);
-                foreach (var page in day3.VisitedPages)
-                    totalPages.Add(page);
+                // 用戶連續使用 3 天，並且造訪不同頁面超過 4 個，就符合標準
+                if (day2 != null && day3 != null)
+                {
+                    var totalPages = new HashSet<string>(day1.VisitedPages);
+                    totalPages.UnionWith(day2.VisitedPages);
+                    totalPages.UnionWith(day3.VisitedPages);
 
-                if(totalPages.Count > 4)
-                    return true;
+                    if (totalPages.Count > 4)
+                        return true;
+                }
             }
 
             return false;
