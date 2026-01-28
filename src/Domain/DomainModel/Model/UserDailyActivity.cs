9187082d913532bb65f497d85437ed48bbb4ba52
Shawn Kuo <were1122was@gmail.com>
2026-01-28 18:59:28 +0800

    modify summary

--- src/Domain/DomainModel/Model/UserDailyActivity.cs
+++ src/Domain/DomainModel/Model/UserDailyActivity.cs
@@ -2,10 +2,14 @@ namespace DomainModel.Model
 {
     public class UserDailyActivity
     {
-        // 只取 TimeStamp 的 .Date 部分
+        /// <summary>
+        /// 只取 TimeStamp 的 .Date 部分
+        /// </summary>
         public DateTime Date { get; set; }
 
-        // 該用戶這一天點過的「所有不重複頁面」
+        /// <summary>
+        /// 該用戶這一天點過的「所有不重複頁面」
+        /// </summary>
         public HashSet<string> VisitedPages { get; set; }
 
         public UserDailyActivity()
